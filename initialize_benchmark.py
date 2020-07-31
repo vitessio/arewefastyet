491289e88fdabf76e5f78be5dce6f72a8045aba7
Akilan <Akilanselva@hotmail.com>
2020-07-31 18:16:50 +0400

    testing test1-packet-inventory.yml

--- initialize_benchmark.py
+++ initialize_benchmark.py
@@ -1,6 +1,5 @@
 from packet_vps import create_vps
 from config import inventory_file
-import uuid 
 import json
 import os
 import yaml
@@ -21,10 +20,29 @@ def init():
     
     with open('ansible/'+inventory_file()) as f:
         data = yaml.load(f, Loader=yaml.FullLoader)
-
-    for key in data:
-        if isinstance(data[key],dict):
-            print(1)
     
+    # Changes ip address with new ip address
+    data = recursive_dict(data,vps[1])
+
+    print(data)
+    
+    with open('ansible/'+inventory_file() , 'w') as f:
+      yaml.dump(data,f)
+    
+
+def recursive_dict(data,ip):
+     for k, v in data.items():
+        if isinstance(data[k],dict) and k == "hosts":
+            data[k] = recursive_dict_ip(data[k],ip)
+        elif isinstance(data[k],dict):
+            data[k] = recursive_dict(data[k],ip)
+     return data
+
+def recursive_dict_ip(data,ip):
+    for k, v in data.items():
+        old_key = k
+        data[ip] = data.pop(old_key)
+    return data
+
 
 init()
