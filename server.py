7d70b5c7ea05b17615531aaada0465e77659c057
Akilan <Akilanselva@hotmail.com>
2020-08-03 22:22:44 +0400

    added filer parameter for the api

--- server.py
+++ server.py
@@ -95,5 +95,90 @@ def all_results():
     
 
     return jsonify(data)
+
+
+# Returns all information in the database 
+@app.route('/filter_results')
+def filter_results():
+    date = request.args.get('date')
+    commit = request.args.get('commit')
+    test_no = request.args.get('test_no') 
+
+    conn = mysql_connect()
+    mycursor = conn.cursor()
     
+    if test_no != None:
+        # Basic run info
+        sql = "SELECT * FROM benchmark where test_no=%s;"
+        adr = (test_no, )
+    else:
+       if date != None and commit != None:
+           sql = "SELECT * FROM benchmark where DateTime BETWEEN %s AND %s AND commit=%s;"
+           adr = (date + ' 00:00:00', date + ' 23:59:59', commit,)
+       elif date != None:
+           sql = "SELECT * FROM benchmark where DateTime BETWEEN %s AND %s;"
+           adr = (date + ' 00:00:00', date + ' 23:59:59',)
+       elif commit != None:
+           sql = "SELECT * FROM benchmark where commit=%s;"
+           adr = (commit,)
+       else:
+           return 'use /allresults to view all results'
+    
+    mycursor.execute(sql,adr)
+        
+    benchmark = mycursor.fetchall()
+    data = {}
+    data['benchmark'] = [] 
+
+    for i in range(len(benchmark)):
+        oltp = []
+        # Oltp information 
+
+        sql = "SELECT * FROM OLTP where test_no = %s;"
+        adr = (benchmark[i][0], )
+        mycursor.execute(sql,adr)
+        
+        oltp_result = mycursor.fetchall()
+
+        for j in range(len(oltp_result)):
+           qps = []
+           sql = "SELECT * FROM qps where OLTP_no = %s;"
+           adr = (oltp_result[j][0], )
+           mycursor.execute(sql,adr)
+
+           qps_result = mycursor.fetchall()
+
+           for k in range(len(qps_result)):
+               qps.append({
+                   'qps_no': qps_result[k][0],
+                   'TPCC_no': qps_result[k][1],
+                   'total_qps': str(qps_result[k][2]),
+                   'reads_qps': str(qps_result[k][3]),
+                   'writes_qps':str(qps_result[k][4]),
+                   'other_qps': str(qps_result[k][5]),
+                   'OLTP_no': qps_result[k][6]
+               })
+
+           oltp.append({
+             'oltp_no': oltp_result[j][0],
+             'test_no': oltp_result[j][1],
+             'tps': str(oltp_result[j][2]),
+             'latency': str(oltp_result[j][3]),
+             'errors': str(oltp_result[j][4]),
+             'reconnects': str(oltp_result[j][5]),
+             'time': oltp_result[j][6],
+             'threads': oltp_result[j][7],
+             'qps': qps
+           })
+
+        data['benchmark'].append({
+        'id':benchmark[i][0],
+        'commit':benchmark[i][1],
+        'datetime':benchmark[i][2],
+        'oltp':oltp
+        })
+        
+    
+
+    return jsonify(data)
    
