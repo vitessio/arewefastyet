b0edbd49f2925fb3bff76b2cb2b47421619a535d
Akilan <Akilanselva@hotmail.com>
2020-09-01 17:13:10 +0400

    graph UI and data intergration with Jinja2 complete for OLTP

--- server.py
+++ server.py
@@ -43,7 +43,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template("index.html")
+    data = graph_data()
+    return render_template("index.html",data=data)
 
 # -------------------------------------------------------------------------------------------------------------------------------------------------------
 # ------------------------------------------------------------- Render Search_Compare -------------------------------------------------------------------
@@ -346,7 +347,71 @@ def filter_results():
     return jsonify(data)
 
 # -----------------------------------------------------------------------------------------------------------------------------------------------------------------
-# ----------------------------------------------------------- Returns results based on commit hash ----------------------------------------------------------------
+# ----------------------------------------------------------- Returns results for the last 7 days ----------------------------------------------------------------
+
+def graph_data():
+    conn = mysql_connect()
+    mycursor = conn.cursor()
+
+    sql = "SELECT * FROM benchmark WHERE DateTime BETWEEN DATE(NOW()) - INTERVAL 7 DAY AND DATE(NOW()) ORDER BY DateTime DESC;"
+    mycursor.execute(sql)
+        
+    benchmark = mycursor.fetchall()
+    data = {}
+    data['benchmark'] = [] 
+
+    for i in range(len(benchmark)):
+        oltp = []
+        # Oltp information 
+
+        sql = "SELECT * FROM OLTP where test_no = %s;"
+        adr = (benchmark[i][0], )
+        mycursor.execute(sql,adr)
+        
+        oltp_result = mycursor.fetchall()
+
+        for j in range(len(oltp_result)):
+           qps = []
+           sql = "SELECT * FROM qps where OLTP_no = %s;"
+           adr = (oltp_result[j][0], )
+           mycursor.execute(sql,adr)
+
+           qps_result = mycursor.fetchall()
+
+           for k in range(len(qps_result)):
+               qps.append({
+                   'qps_no': qps_result[k][0],
+                   #'TPCC_no': qps_result[k][1],
+                   'total_qps': str(qps_result[k][2]),
+                   'reads_qps': str(qps_result[k][3]),
+                   'writes_qps':str(qps_result[k][4]), 
+                   'other_qps': str(qps_result[k][5]),
+                   'OLTP_no': qps_result[k][6]
+               })
+
+           oltp.append({
+             'oltp_no': oltp_result[j][0],
+             'test_no': oltp_result[j][1],
+             'tps': str(oltp_result[j][2]),
+             'latency': str(oltp_result[j][3]),
+             'errors': str(oltp_result[j][4]),
+             'reconnects': str(oltp_result[j][5]),
+             'time': oltp_result[j][6],
+             'threads': oltp_result[j][7],
+             'qps': qps
+           })
+
+        data['benchmark'].append({
+        'id':benchmark[i][0],
+        'commit':benchmark[i][1],
+        #'datetime':benchmark[i][2],
+        'oltp':oltp
+        })
+         
+        return data
+
+# -----------------------------------------------------------------------------------------------------------------------------------------------------------------
+# ------------------------------------------------------------ Returns results based on commit hash ---------------------------------------------------------------
 
 def search_commit(commit):
     conn = mysql_connect()
