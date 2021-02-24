05fd7410e093241f6dab035fecc4a7d0e05bca3c
Florent Poinsard <florent.poinsard@outlook.fr>
2021-02-24 14:31:30 +0100

    Addition of setuptools
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+setuptools.setup(
+    name="Vitess Benchmark",
+    version="0.1",
+    author="vitessio",
+    url="https://github.com/vitessio/arewefastyet",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    entry_points={
+        'console_scripts': [
+            'clibench = bench_cli.cli:main',
+        ],
+    },
+    packages=setuptools.find_packages(),
+    python_requires='>=3.7',
+)
