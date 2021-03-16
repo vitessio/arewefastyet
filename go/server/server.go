69e1d7994e27a6d5f3fd35c9ee001b6187eebea7
Akilan1999 <akilan.selvacoumar@planetscale.com>
2021-03-16 14:15:17 +0400

    added base template files
    Signed-off-by: Akilan1999 <akilan.selvacoumar@planetscale.com>

--- go/server/server.go
+++ go/server/server.go
@@ -17,6 +17,31 @@ func main() {
 			"title": "Vitess benchmark",
 		})
 	})
+
+	//Home page
+	router.GET("/", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", gin.H{
+			"title": "Vitess benchmark",
+		})
+	})
+
+	//Request benchmark page
+	router.GET("/search_compare", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "search_compare.tmpl", gin.H{
+			"title": "Vitess benchmark",
+		})
+	})
+
+	//Request benchmark page
+	router.GET("/request_benchmark", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "request_benchmark.tmpl", gin.H{
+			"title": "Vitess benchmark",
+		})
+	})
+
+
+
+
 	router.Run(":8080")
 
 }
