b2835c7cd4d01a3fcdbeb0958ee30ca9a66fc314
Akilan1999 <akilan.selvacoumar@planetscale.com>
2021-03-18 11:55:20 +0400

    added upload file method for slack channel
    Signed-off-by: Akilan1999 <akilan.selvacoumar@planetscale.com>

--- go/slack/slack.go
+++ go/slack/slack.go
@@ -1,29 +1,59 @@
-package main
+package slack
 
-import(
-  "fmt"
-  "github.com/slack-go/slack"
+import (
+	"fmt"
+	"github.com/slack-go/slack"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"os"
 )
 
-func main(){
-  api := slack.New("xoxb-23846488290-1288404371285-bizzoEHUDRO94eowq4GOykIR")
+type conf struct {
+	Token   string `yaml:"slack_api_token"`
+	Channel string `yaml:"slack_channel"`
+}
 
-  // If you set debugging, it will log all requests to the console
-	// Useful when encountering issues
-	// slack.New("YOUR_TOKEN_HERE", slack.OptionDebug(true))
+func UploadFile() error {
+	c, err := GetCredentials()
 
+	if err != nil {
+		return err
+	}
 
-  channelID, timestamp, err := api.PostMessage(
-    "bot-benchmarks",
-    slack.MsgOptionText("test", false),
-    slack.MsgOptionAsUser(true), // Add this if you want that the bot would post message as a user, otherwise it will send response using the default slackbot
-  )
-  if err != nil {
-    fmt.Printf("%s\n", err)
-    return
-  }
-  fmt.Printf("Message successfully sent to channel %s at %s", channelID, timestamp)
+	api := slack.New(c.Token)
+
+	params := slack.FileUploadParameters{
+		Title:          "OlTP ",
+		Filetype:       "json",
+		File:           "report/sample/sample_oltp.json",
+		Channels:       []string{c.Channel},
+		InitialComment: "This is sample OLTP",
+	}
+
+	_, err = api.UploadFile(params)
+	if err != nil {
+		return err
+	}
+
+	return nil
 
 }
 
+func GetCredentials() (*conf, error) {
 
+	//Read from the config file
+
+	var c conf
+	fmt.Println(os.Getwd())
+	yamlFile, err := ioutil.ReadFile("config/config.yaml")
+	if err != nil {
+		return nil, err
+	}
+	err = yaml.Unmarshal(yamlFile, &c)
+	if err != nil {
+		return nil, err
+	}
+
+	return &c, nil
+
+}
