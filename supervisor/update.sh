3f2f0db3f302af439e1a5ef83fcb7ab49925fcff
Florent Poinsard <florent.poinsard@outlook.fr>
2021-03-26 10:57:25 +0100

    Modified target directory in supervisor cp
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>

--- supervisor/update.sh
+++ supervisor/update.sh
@@ -3,6 +3,6 @@
 # shellcheck disable=SC2046
 supervisord_file="$(dirname $(realpath $0))/supervisord.conf"
 
-cp $supervisord_file /etc/supervisord/supervisord.conf
+cp $supervisord_file /etc/supervisor/supervisord.conf
 
 supervisorctl reload all
