8ab1d4a292c8f2dd9757aa97d924850dab03bdd9
Florent Poinsard <florent.poinsard@outlook.fr>
2021-03-31 16:40:27 +0200

    Addition of configuration struct in slack package
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>

---
+++ go/slack/config.go
@@ -0,0 +1,42 @@
+/*
+ *
+ * Copyright 2021 The Vitess Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * /
+ */
+
+package slack
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+const (
+	flagToken   = "slack-token"
+	flagChannel = "slack-channel"
+)
+
+type Config struct {
+	Token   string
+	Channel string
+}
+
+func (c *Config) AddToCommand(cmd *cobra.Command) {
+	cmd.Flags().StringVar(&c.Token, flagToken, "", "Token used to authenticate Slack")
+	cmd.Flags().StringVar(&c.Token, flagChannel, "", "Slack channel on which to post messages")
+	
+	_ = viper.BindPFlag(flagToken, cmd.Flags().Lookup(flagToken))
+	_ = viper.BindPFlag(flagChannel, cmd.Flags().Lookup(flagChannel))
+}
