a9b45f86ee9fa9fb820bc6a2a76bb828fc10ba1f
Florent Poinsard <florent.poinsard@outlook.fr>
2021-04-08 08:11:49 +0200

    Addition of macrobench's cmd base
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>

--- go/cmd/root.go
+++ go/cmd/root.go
@@ -22,6 +22,7 @@ import (
 	"github.com/vitessio/arewefastyet/go/cmd/exec"
 	"github.com/vitessio/arewefastyet/go/cmd/gen"
 	"github.com/vitessio/arewefastyet/go/cmd/infra"
+	"github.com/vitessio/arewefastyet/go/cmd/macrobench"
 	"github.com/vitessio/arewefastyet/go/cmd/microbench"
 	"github.com/vitessio/arewefastyet/go/cmd/web"
 	"log"
@@ -59,6 +60,7 @@ func init() {
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 	rootCmd.AddCommand(microbench.MicroBenchCmd())
+	rootCmd.AddCommand(macrobench.MacroBenchCmd())
 	rootCmd.AddCommand(infra.InfraCmd())
 	rootCmd.AddCommand(web.WebCmd())
 	rootCmd.AddCommand(exec.ExecCmd())
