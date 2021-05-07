d7b2151cd6268c211bea6e1a1e486f163050cdef
GuptaManan100 <manan@planetscale.com>
2021-05-07 13:06:26 +0530

    added foreign key to microbenchmark details
    Signed-off-by: GuptaManan100 <manan@planetscale.com>

--- sql/vitess-benchmark.sql
+++ sql/vitess-benchmark.sql
@@ -1,92 +1,142 @@
-CREATE DATABASE IF NOT EXISTS benchmark;
+/*
+ *
+ * Copyright 2021 The Vitess Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * /
+ */
 
-USE benchmark;
+--
+-- Table structure for table `OLTP`
+--
 
-CREATE TABLE IF NOT EXISTS execution (
-    `uuid` VARCHAR(100) NOT NULL,
-    `status` VARCHAR(100) DEFAULT 'created',
-    `started_at` DATETIME NULL,
-    `finished_at` DATETIME NULL,
-    `source` VARCHAR(100) DEFAULT NULL,
-    `git_ref` VARCHAR(100) DEFAULT NULL,
-    PRIMARY KEY (`uuid`)
+DROP TABLE IF EXISTS `OLTP`;
+CREATE TABLE `OLTP` (
+                        `OLTP_no` int(11) NOT NULL AUTO_INCREMENT,
+                        `macrobenchmark_id` int(11) DEFAULT NULL,
+                        `tps` decimal(8,2) DEFAULT NULL,
+                        `latency` decimal(8,2) DEFAULT NULL,
+                        `errors` decimal(8,2) DEFAULT NULL,
+                        `reconnects` decimal(8,2) DEFAULT NULL,
+                        `time` int(11) DEFAULT NULL,
+                        `threads` decimal(8,2) DEFAULT NULL,
+                        PRIMARY KEY (`OLTP_no`),
+                        KEY `test_no` (`macrobenchmark_id`),
+                        CONSTRAINT `OLTP_ibfk_1` FOREIGN KEY (`macrobenchmark_id`) REFERENCES `macrobenchmark` (`macrobenchmark_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=511 DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `TPCC`
+--
+
+DROP TABLE IF EXISTS `TPCC`;
+CREATE TABLE `TPCC` (
+                        `TPCC_no` int(11) NOT NULL AUTO_INCREMENT,
+                        `macrobenchmark_id` int(11) DEFAULT NULL,
+                        `tps` decimal(8,2) DEFAULT NULL,
+                        `latency` decimal(8,2) DEFAULT NULL,
+                        `errors` decimal(8,2) DEFAULT NULL,
+                        `reconnects` decimal(8,2) DEFAULT NULL,
+                        `time` int(11) DEFAULT NULL,
+                        `threads` decimal(8,2) DEFAULT NULL,
+                        PRIMARY KEY (`TPCC_no`),
+                        KEY `test_no` (`macrobenchmark_id`),
+                        CONSTRAINT `TPCC_ibfk_1` FOREIGN KEY (`macrobenchmark_id`) REFERENCES `macrobenchmark` (`macrobenchmark_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=356 DEFAULT CHARSET=utf8;
+
+--
+-- Table structure for table `execution`
+--
+
+DROP TABLE IF EXISTS `execution`;
+CREATE TABLE `execution` (
+                             `uuid` varchar(100) NOT NULL,
+                             `status` varchar(100) DEFAULT 'created',
+                             `started_at` datetime DEFAULT NULL,
+                             `finished_at` datetime DEFAULT NULL,
+                             `source` varchar(100) DEFAULT NULL,
+                             `git_ref` varchar(100) DEFAULT NULL,
+                             PRIMARY KEY (`uuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS macrobenchmark (
-  `macrobenchmark_id` int(11) NOT NULL AUTO_INCREMENT,
-  `exec_uuid` VARCHAR(100) DEFAULT NULL,
-  `commit` varchar(100) DEFAULT NULL,
-  `DateTime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `source` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`macrobenchmark_id`),
-  KEY `exec_uuid` (`exec_uuid`),
-  CONSTRAINT `macrobenchmark_ibfk_1` FOREIGN KEY (`exec_uuid`) REFERENCES execution (`uuid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+--
+-- Table structure for table `macrobenchmark`
+--
 
-CREATE TABLE IF NOT EXISTS `OLTP` (
-  `OLTP_no` int(11) NOT NULL AUTO_INCREMENT,
-  `macrobenchmark_id` int(11) DEFAULT NULL,
-  `tps` decimal(8,2) DEFAULT NULL,
-  `latency` decimal(8,2) DEFAULT NULL,
-  `errors` decimal(8,2) DEFAULT NULL,
-  `reconnects` decimal(8,2) DEFAULT NULL,
-  `time` int(11) DEFAULT NULL,
-  `threads` decimal(8,2) DEFAULT NULL,
-  PRIMARY KEY (`OLTP_no`),
-  KEY `macrobenchmark_id` (`macrobenchmark_id`),
-  CONSTRAINT `OLTP_ibfk_1` FOREIGN KEY (`macrobenchmark_id`) REFERENCES macrobenchmark (`macrobenchmark_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `macrobenchmark`;
+CREATE TABLE `macrobenchmark` (
+                                  `macrobenchmark_id` int(11) NOT NULL AUTO_INCREMENT,
+                                  `commit` varchar(100) DEFAULT NULL,
+                                  `DateTime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+                                  `source` varchar(100) DEFAULT NULL,
+                                  `exec_uuid` varchar(100) DEFAULT NULL,
+                                  PRIMARY KEY (`macrobenchmark_id`),
+                                  KEY `macrobenchmark_ibfk_1` (`exec_uuid`),
+                                  CONSTRAINT `macrobenchmark_ibfk_1` FOREIGN KEY (`exec_uuid`) REFERENCES `execution` (`uuid`)
+) ENGINE=InnoDB AUTO_INCREMENT=939 DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS `TPCC` (
-  `TPCC_no` int(11) NOT NULL AUTO_INCREMENT,
-  `macrobenchmark_id` int(11) DEFAULT NULL,
-  `tps` decimal(8,2) DEFAULT NULL,
-  `latency` decimal(8,2) DEFAULT NULL,
-  `errors` decimal(8,2) DEFAULT NULL,
-  `reconnects` decimal(8,2) DEFAULT NULL,
-  `time` int(11) DEFAULT NULL,
-  `threads` decimal(8,2) DEFAULT NULL,
-  PRIMARY KEY (`TPCC_no`),
-  KEY `macrobenchmark_id` (`macrobenchmark_id`),
-  CONSTRAINT `TPCC_ibfk_1` FOREIGN KEY (`macrobenchmark_id`) REFERENCES macrobenchmark (`macrobenchmark_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+--
+-- Table structure for table `microbenchmark`
+--
 
+DROP TABLE IF EXISTS `microbenchmark`;
+CREATE TABLE `microbenchmark` (
+                                  `microbenchmark_no` int(11) NOT NULL AUTO_INCREMENT,
+                                  `pkg_name` varchar(255) DEFAULT NULL,
+                                  `name` varchar(255) DEFAULT NULL,
+                                  `git_ref` varchar(255) DEFAULT NULL,
+                                  `exec_uuid` varchar(100) DEFAULT NULL,
+                                  PRIMARY KEY (`microbenchmark_no`),
+                                  KEY `microbenchmark_ibfk_1` (`exec_uuid`),
+                                  CONSTRAINT `microbenchmark_ibfk_1` FOREIGN KEY (`exec_uuid`) REFERENCES `execution` (`uuid`)
+) ENGINE=InnoDB AUTO_INCREMENT=14619 DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS `qps` (
-  `qps_no` int(11) NOT NULL AUTO_INCREMENT,
-  `TPCC_no` int(11) DEFAULT NULL,
-  `total_qps` decimal(8,2) DEFAULT NULL,
-  `reads_qps` decimal(8,2) DEFAULT NULL,
-  `writes_qps` decimal(8,2) DEFAULT NULL,
-  `other_qps` decimal(8,2) DEFAULT NULL,
-  `OLTP_no` int(11) DEFAULT NULL,
-  PRIMARY KEY (`qps_no`),
-  KEY `TPCC_no` (`TPCC_no`),
-  KEY `OLTP_no` (`OLTP_no`),
-  CONSTRAINT `qps_ibfk_1` FOREIGN KEY (`TPCC_no`) REFERENCES `TPCC` (`TPCC_no`),
-  CONSTRAINT `qps_ibfk_2` FOREIGN KEY (`OLTP_no`) REFERENCES `OLTP` (`OLTP_no`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+--
+-- Table structure for table `microbenchmark_details`
+--
 
-CREATE TABLE IF NOT EXISTS `microbenchmark` (
-    `microbenchmark_no` INT AUTO_INCREMENT,
-    `exec_uuid` VARCHAR(100) DEFAULT NULL,
-    `pkg_name` VARCHAR(255),
-    `name` VARCHAR(255),
-    `git_ref` VARCHAR(255),
-    PRIMARY KEY (`microbenchmark_no`),
-    KEY `exec_uuid` (`exec_uuid`),
-    CONSTRAINT `microbenchmark_ibfk_1` FOREIGN KEY (`exec_uuid`) REFERENCES execution (`uuid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS `microbenchmark_details`;
+CREATE TABLE `microbenchmark_details` (
+                                          `detail_no` int(11) NOT NULL AUTO_INCREMENT,
+                                          `microbenchmark_no` int(11) DEFAULT NULL,
+                                          `name` varchar(255) DEFAULT NULL,
+                                          `bench_type` varchar(255) DEFAULT NULL,
+                                          `n` int(11) DEFAULT NULL,
+                                          `ns_per_op` decimal(22,5) DEFAULT NULL,
+                                          `mb_per_sec` decimal(22,5) DEFAULT NULL,
+                                          `bytes_per_op` decimal(22,5) DEFAULT NULL,
+                                          `allocs_per_op` decimal(22,5) DEFAULT NULL,
+                                          PRIMARY KEY (`detail_no`),
+                                          KEY `microbenchmark_details_fk_1` (`microbenchmark_no`),
+                                          CONSTRAINT `microbenchmark_details_fk_1` FOREIGN KEY (`microbenchmark_no`) REFERENCES `microbenchmark` (`microbenchmark_no`)
+) ENGINE=InnoDB AUTO_INCREMENT=358174 DEFAULT CHARSET=utf8;
 
-CREATE TABLE IF NOT EXISTS `microbenchmark_details` (
-    `detail_no` INT AUTO_INCREMENT,
-    `microbenchmark_no` INT,
-    `name` VARCHAR(255),
-    `bench_type` VARCHAR(255),
-    `n` INT,
-    `ns_per_op` DECIMAL(22,5),
-    `mb_per_sec` DECIMAL(22,5),
-    `bytes_per_op` DECIMAL(22,5),
-    `allocs_per_op` DECIMAL(22,5),
-    PRIMARY KEY (`detail_no`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+--
+-- Table structure for table `qps`
+--
+
+DROP TABLE IF EXISTS `qps`;
+CREATE TABLE `qps` (
+                       `qps_no` int(11) NOT NULL AUTO_INCREMENT,
+                       `TPCC_no` int(11) DEFAULT NULL,
+                       `total_qps` decimal(8,2) DEFAULT NULL,
+                       `reads_qps` decimal(8,2) DEFAULT NULL,
+                       `writes_qps` decimal(8,2) DEFAULT NULL,
+                       `other_qps` decimal(8,2) DEFAULT NULL,
+                       `OLTP_no` int(11) DEFAULT NULL,
+                       PRIMARY KEY (`qps_no`),
+                       KEY `TPCC_no` (`TPCC_no`),
+                       KEY `OLTP_no` (`OLTP_no`),
+                       CONSTRAINT `qps_ibfk_1` FOREIGN KEY (`TPCC_no`) REFERENCES `TPCC` (`TPCC_no`),
+                       CONSTRAINT `qps_ibfk_2` FOREIGN KEY (`OLTP_no`) REFERENCES `OLTP` (`OLTP_no`)
+) ENGINE=InnoDB AUTO_INCREMENT=865 DEFAULT CHARSET=utf8;
