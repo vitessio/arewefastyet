ae5aa9bc623fe2f8e9cbb22e42f499867cd5e3ae
Florent Poinsard <florent.poinsard@outlook.fr>
2021-06-04 11:53:38 +0200

    Addition of SQL DML in psdb package
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>

--- go/storage/psdb/psdb.go
+++ go/storage/psdb/psdb.go
@@ -19,7 +19,11 @@
 package psdb
 
 import (
+	"context"
+	"database/sql"
+	"errors"
 	"github.com/planetscale/planetscale-go/planetscale"
+	"github.com/planetscale/planetscale-go/planetscale/dbutil"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
@@ -28,6 +32,10 @@ const (
 	flagPsdbOrg              = "planetscale-db-org"
 	flagPsdbServiceTokenName = "planetscale-db-service-token-name"
 	flagPsdbServiceToken     = "planetscale-db-service-token"
+	flagPsdbDatabase         = "planetscale-db-database"
+	flagPsdbBranch           = "planetscale-db-branch"
+
+	ErrorClientConnectionNotInitialized = "the client connection to the database is not initialized"
 )
 
 type (
@@ -38,12 +46,15 @@ type (
 
 	Config struct {
 		ServiceToken
-		Org string
+		Org      string
+		Database string
+		Branch   string
 	}
 
 	Client struct {
 		Config *Config
 		client *planetscale.Client
+		dial   *sql.DB
 	}
 )
 
@@ -51,16 +62,22 @@ func (cfg *Config) AddToViper(v *viper.Viper) {
 	_ = v.UnmarshalKey(flagPsdbOrg, &cfg.Org)
 	_ = v.UnmarshalKey(flagPsdbServiceTokenName, &cfg.Name)
 	_ = v.UnmarshalKey(flagPsdbServiceToken, &cfg.Token)
+	_ = v.UnmarshalKey(flagPsdbDatabase, &cfg.Database)
+	_ = v.UnmarshalKey(flagPsdbBranch, &cfg.Branch)
 }
 
 func (cfg *Config) AddToCommand(cmd *cobra.Command) {
 	cmd.Flags().StringVar(&cfg.Org, flagPsdbOrg, "", "Name of the PlanetscaleDB organization.")
 	cmd.Flags().StringVar(&cfg.Name, flagPsdbServiceTokenName, "", "PlanetscaleDB service token name.")
 	cmd.Flags().StringVar(&cfg.Token, flagPsdbServiceToken, "", "PlanetscaleDB service token value.")
+	cmd.Flags().StringVar(&cfg.Database, flagPsdbDatabase, "", "PlanetscaleDB database name.")
+	cmd.Flags().StringVar(&cfg.Branch, flagPsdbBranch, "main", "PlanetscaleDB branch to use.")
 
 	_ = viper.BindPFlag(flagPsdbOrg, cmd.Flags().Lookup(flagPsdbOrg))
 	_ = viper.BindPFlag(flagPsdbServiceTokenName, cmd.Flags().Lookup(flagPsdbServiceTokenName))
 	_ = viper.BindPFlag(flagPsdbServiceToken, cmd.Flags().Lookup(flagPsdbServiceToken))
+	_ = viper.BindPFlag(flagPsdbDatabase, cmd.Flags().Lookup(flagPsdbDatabase))
+	_ = viper.BindPFlag(flagPsdbBranch, cmd.Flags().Lookup(flagPsdbBranch))
 }
 
 func (cfg Config) NewClient() (*Client, error) {
@@ -74,5 +91,53 @@ func (cfg Config) NewClient() (*Client, error) {
 		return nil, err
 	}
 	client.client = psdbClient
+
+	dialCfg := &dbutil.DialConfig{
+		Organization: cfg.Org,
+		Database:     cfg.Database,
+		Branch:       cfg.Branch,
+		Client:       psdbClient,
+	}
+	db, err := dbutil.Dial(context.Background(), dialCfg)
+	if err != nil {
+		return nil, err
+	}
+	client.dial = db
 	return client, nil
 }
+
+func (c *Client) Close() error {
+	if c.dial == nil {
+		return errors.New(ErrorClientConnectionNotInitialized)
+	}
+	return c.dial.Close()
+}
+
+func (c *Client) Insert(query string, args ...interface{}) (int64, error) {
+	if c.dial == nil {
+		return 0, errors.New(ErrorClientConnectionNotInitialized)
+	}
+	stms, err := c.dial.Prepare(query)
+	if err != nil {
+		return 0, err
+	}
+	defer stms.Close()
+
+	res, err := stms.Exec(args...)
+	if err != nil {
+		return 0, err
+	}
+	return res.LastInsertId()
+}
+
+func (c *Client) Select(query string, args ...interface{}) (*sql.Rows, error) {
+	if c.dial == nil {
+		return nil, errors.New(ErrorClientConnectionNotInitialized)
+	}
+	rows, err := c.dial.Query(query, args...)
+	if err != nil {
+		return nil, err
+	}
+	return rows, nil
+}
+
