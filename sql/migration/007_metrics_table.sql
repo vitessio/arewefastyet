3e377d0a86c637dd4500de40deffcc505c57dfca
Florent Poinsard <florent.poinsard@outlook.fr>
2021-06-15 18:53:14 +0200

    Addition of the new metrics table to the SQL schema
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>

---
+++ sql/migration/007_metrics_table.sql
@@ -0,0 +1,30 @@
+/*
+ *
+ * Copyright 2021 The Vitess Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * /
+ */
+
+--
+-- Table structure for table `metrics`
+--
+
+DROP TABLE IF EXISTS `metrics`;
+CREATE TABLE `metrics` (
+                           `id` INT(11) NOT NULL AUTO_INCREMENT,
+                           `exec_uuid` VARCHAR(100) DEFAULT NULL,
+                           `name` VARCHAR(250) DEFAULT NULL,
+                           `value` DECIMAL(8,2) DEFAULT NULL,
+                           PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
