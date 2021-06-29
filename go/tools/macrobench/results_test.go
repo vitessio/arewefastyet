2641413d784941e5483f9c1f91dd703bcfb63c4e
Florent Poinsard <florent.poinsard@outlook.fr>
2021-06-29 14:57:19 +0200

    Fixed improved by % for the macrobenchmark errors
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>

--- go/tools/macrobench/results_test.go
+++ go/tools/macrobench/results_test.go
@@ -155,7 +155,7 @@ func TestCompareDetailsArrays(t *testing.T) {
 	resultOfOne := *newResult(qpsOfOne, 1.0, 1.0, 1.0, 1.0, 1, 1.0)
 	resultOfTwo := *newResult(qpsOfTwo, 2.0, 2.0, 2.0, 2.0, 2, 2.0)
 	qpsOfFifty := *newQPS(-100, -100, -100, -100)
-	resultOfFifty := *newResult(qpsOfFifty, -100, 50, -100, -100, -100, -100)
+	resultOfFifty := *newResult(qpsOfFifty, -100, 50, 50, -100, -100, -100)
 
 	tests := []struct {
 		name         string
@@ -197,7 +197,7 @@ func TestCompareDetailsArrays(t *testing.T) {
 			Comparison{
 				Reference:   *newDetails(*newBenchmarkID(4, "webhook", nil), "f78gh1p", resultOfTwo, metrics.ExecutionMetrics{ComponentsCPUTime: map[string]float64{}, ComponentsMemStatsAllocBytes: map[string]float64{}}),
 				Compare:     *newDetails(*newBenchmarkID(3, "api_call", nil), "f78gh1p", resultOfOne, metrics.ExecutionMetrics{ComponentsCPUTime: map[string]float64{}, ComponentsMemStatsAllocBytes: map[string]float64{}}),
-				Diff:        *newResult(*newQPS(50, 50, 50, 50), 50, -100, 50, 50, 50, 50),
+				Diff:        *newResult(*newQPS(50, 50, 50, 50), 50, -100, -100, 50, 50, 50),
 				DiffMetrics: metrics.ExecutionMetrics{ComponentsCPUTime: map[string]float64{}, ComponentsMemStatsAllocBytes: map[string]float64{}},
 			},
 		}},
