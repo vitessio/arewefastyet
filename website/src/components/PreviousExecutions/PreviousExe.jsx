6893a21fa9fe46c45e12879bca55551425b6b0c8
Camillemtd <camille_mtd@hotmail.com>
2023-06-06 11:08:00 +0200

    Formatting the date in the PreviousExe component with moment.js librairy

--- website/src/components/PreviousExecutions/PreviousExe.jsx
+++ website/src/components/PreviousExecutions/PreviousExe.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import moment from 'moment';
 
 import '../PreviousExecutions/previousexe.css'
 
 const PreviousExe = ({data}) => {
+        /*BACKGROUND STATUS*/ 
 
     const getStatusClass = (status) => {
         if (status === 'finished') {
@@ -15,14 +17,24 @@ const PreviousExe = ({data}) => {
             return 'default';
         }
     }
+
+        //Create a Moment object from the given date string
+        
+        const startedDate = moment(data.started_at)
+        const finishedDate = moment(data.finished_at)
+        // Format the date using the format method of Moment.js
+        // Here, we use 'DD/MM/YYYY HH:mm:ss' as the desired format
+
+        const formattedStartedDate = startedDate.format('MM/DD/YYYY HH:mm')
+        const formattedFinishedDate = finishedDate.format('MM/DD/YYYY HH:mm')
     return (
     <>
         <tr>
             <td>{data.uuid.slice(0, 8)}</td>
             <td><a target='_blank' href={`https://github.com/vitessio/vitess/commit/${data.git_ref}`}>{data.git_ref.slice(0,6)}</a></td>
             <td>{data.source}</td>
-            <td>{data.started_at}</td>
-            <td>{data.finished_at}</td>
+            <td>{formattedStartedDate}</td>
+            <td>{formattedFinishedDate}</td>
             <td>{data.type_of.slice(0,4)}</td>
             <td className='tdPR'>{data.pull_nb}</td>
             <td>{data.golang_version}</td>
