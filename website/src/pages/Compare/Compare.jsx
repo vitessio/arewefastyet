a01806aa7c1f327adae969a260369ae3bffbff6c
Camillemtd <camille_mtd@hotmail.com>
2023-06-26 09:35:10 +0200

    top compare page

--- website/src/pages/Compare/Compare.jsx
+++ website/src/pages/Compare/Compare.jsx
@@ -20,8 +20,22 @@ import '../Compare/compare.css'
 
 const Compare = () => {
     return (
-        <div>
-            Compare
+        <div className='compare'>
+            <div className='compare__top justify--content'>
+                <div className='compare__top__text'>
+                    <h2>Compare</h2>
+                    <span>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer a augue mi.
+                            Etiam sed imperdiet ligula, vel elementum velit.
+                            Phasellus sodales felis eu condimentum convallis.
+                            Suspendisse sodales malesuada iaculis. Mauris molestie placerat ex non malesuada.
+                            Curabitur eget sagittis eros. Aliquam aliquam sem non tincidunt volutpat. 
+                    </span>
+                    
+                </div>
+                <figure className='compareStats'></figure>
+            </div>
+            <figure className='line'></figure>
         </div>
     );
 };
