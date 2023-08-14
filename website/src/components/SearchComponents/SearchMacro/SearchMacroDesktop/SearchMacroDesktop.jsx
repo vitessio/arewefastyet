089b0fc28e7ae17c5403fefa5c11046a28dc28fe
Camillemtd <camille_mtd@hotmail.com>
2023-08-14 18:21:05 +0200

    NewSearchComponent

--- website/src/components/SearchComponents/SearchMacro/SearchMacroDesktop/SearchMacroDesktop.jsx
+++ website/src/components/SearchComponents/SearchMacro/SearchMacroDesktop/SearchMacroDesktop.jsx
@@ -19,41 +19,237 @@ import PropTypes from "prop-types";
 
 import "../SearchMacroDesktop/searchMacroDesktop.css";
 
-import { fixed } from "../../../../utils/Utils";
+import { fixed, formatByteForGB } from "../../../../utils/Utils";
 
-const SearchMacroDesktop = ({ data }) => {
-  const renderZeroSpans = Array(16)
-    .fill(0)
-    .map((_, index) => <span key={index}>0</span>);
-    
+const SearchMacroDesktop = ({ data, gitRef }) => {
+  const renderZeroSpans = <span>0</span>;
+
   return (
     <div className="flex--column searchMAcro__desktop__data">
-      {data[1] ? (
-        <>
-          <span>{fixed(data[1][0].Result.qps.total, 2)}</span>
-          <span>{data[1][0].Result.qps.reads}</span>
-          <span>{fixed(data[1][0].Result.qps.writes, 2)}</span>
-          <span>{data[1][0].Result.qps.other.toFixed(2)}</span>
-          <span>{fixed(data[1][0].Result.tps, 2)}</span>
-          <span>{data[1][0].Result.latency}</span>
-          <span>{fixed(data[1][0].Result.errors, 2)}</span>
-          <span>{data[1][0].Result.reconnects}</span>
-          <span>{data[1][0].Result.time}</span>
-          <span>{data[1][0].Result.threads}</span>
-          <span>{data[1][0].Metrics.TotalComponentsCPUTime.toFixed(0)}</span>
-          <span>{fixed(data[1][0].Metrics.ComponentsCPUTime.vtgate, 2)}</span>
-          <span>{fixed(data[1][0].Metrics.ComponentsCPUTime.vttablet, 2)}</span>
-          <span>{data[1][0].Metrics.TotalComponentsMemStatsAllocBytes}</span>
-          <span>
-            {fixed(data[1][0].Metrics.ComponentsMemStatsAllocBytes.vtgate, 2)}
-          </span>
-          <span>
-            {fixed(data[1][0].Metrics.ComponentsMemStatsAllocBytes.vttablet, 2)}
-          </span>
-        </>
-      ) : (
-        renderZeroSpans
-      )}
+      <div className="searchMAcro__desktop__data__header">
+        <h3>{data[0]}</h3>
+        <a
+          target="_blank"
+          rel="noopener noreferrer"
+          href={`https://github.com/vitessio/vitess/commit/${gitRef}`}
+        >
+          <span>{gitRef}</span>
+        </a>
+      </div>
+      <table>
+        <tbody>
+          <tr>
+            <td className="sidebar--border">
+              <span>QPS Total</span>
+            </td>
+            <td className="datatd">
+              {data[1] ? (
+                <span>{fixed(data[1][0].Result.qps.total, 2)}</span>
+              ) : (
+                renderZeroSpans
+              )}
+            </td>
+          </tr>
+          <tr>
+            <td className="sidebar--border">
+              <span>QPS Reads</span>
+            </td>
+            <td className="datatd">
+              {data[1] ? (
+                <span>{data[1][0].Result.qps.reads}</span>
+              ) : (
+                renderZeroSpans
+              )}
+            </td>
+          </tr>
+          <tr>
+            <td className="sidebar--border">
+              <span>QPS Writes</span>
+            </td>
+            <td className="datatd">
+              {data[1] ? (
+                <span>{fixed(data[1][0].Result.qps.writes, 2)}</span>
+              ) : (
+                renderZeroSpans
+              )}
+            </td>
+          </tr>
+          <tr>
+            <td className="sidebar--border">
+              <span>QPS Other</span>
+            </td>
+            <td className="datatd">
+              {data[1] ? (
+                <span>{data[1][0].Result.qps.other.toFixed(2)}</span>
+              ) : (
+                renderZeroSpans
+              )}
+            </td>
+          </tr>
+          <tr>
+            <td className="sidebar--border">
+              <span>TPS</span>
+            </td>
+            <td className="datatd">
+              {data[1] ? (
+                <span>{fixed(data[1][0].Result.tps, 2)}</span>
+              ) : (
+                renderZeroSpans
+              )}
+            </td>
+          </tr>
+          <tr>
+            <td className="sidebar--border">
+              <span>Latency</span>
+            </td>
+            <td className="datatd">
+              {data[1] ? (
+                <span>{data[1][0].Result.latency}</span>
+              ) : (
+                renderZeroSpans
+              )}
+            </td>
+          </tr>
+          <tr>
+            <td className="sidebar--border">
+              <span>Errors</span>
+            </td>
+            <td className="datatd">
+              {data[1] ? (
+                <span>{fixed(data[1][0].Result.errors, 2)}</span>
+              ) : (
+                renderZeroSpans
+              )}
+            </td>
+          </tr>
+          <tr>
+            <td className="sidebar--border">
+              <span>Reconnects</span>
+            </td>
+            <td className="datatd">
+              {data[1] ? (
+                <span>{data[1][0].Result.reconnects}</span>
+              ) : (
+                renderZeroSpans
+              )}
+            </td>
+          </tr>
+          <tr>
+            <td className="sidebar--border">
+              <span>Time</span>
+            </td>
+            <td className="datatd">
+              {data[1] ? (
+                <span>{data[1][0].Result.time}</span>
+              ) : (
+                renderZeroSpans
+              )}
+            </td>
+          </tr>
+          <tr>
+            <td className="sidebar--border">
+              <span>Threads</span>
+            </td>
+            <td className="datatd">
+              {data[1] ? (
+                <span>{data[1][0].Result.threads}</span>
+              ) : (
+                renderZeroSpans
+              )}
+            </td>
+          </tr>
+          <tr>
+            <td className="sidebar--border">
+              <span>Total CPU time</span>
+            </td>
+            <td className="datatd">
+              {data[1] ? (
+                <span>
+                  {data[1][0].Metrics.TotalComponentsCPUTime.toFixed(0)}
+                </span>
+              ) : (
+                renderZeroSpans
+              )}
+            </td>
+          </tr>
+          <tr>
+            <td className="sidebar--border">
+              <span>CPU time vtgate</span>
+            </td>
+            <td className="datatd">
+              {data[1] ? (
+                <span>
+                  {fixed(data[1][0].Metrics.ComponentsCPUTime.vtgate, 2)}
+                </span>
+              ) : (
+                renderZeroSpans
+              )}
+            </td>
+          </tr>
+          <tr>
+            <td className="sidebar--border">
+              <span>CPU time vttablet</span>
+            </td>
+            <td className="datatd">
+              {data[1] ? (
+                <span>
+                  {fixed(data[1][0].Metrics.ComponentsCPUTime.vttablet, 2)}
+                </span>
+              ) : (
+                renderZeroSpans
+              )}
+            </td>
+          </tr>
+          <tr>
+            <td className="sidebar--border">
+              <span>Total Allocs bytes</span>
+            </td>
+            <td className="datatd">
+              {data[1] ? (
+                <span>
+                  {formatByteForGB(
+                    data[1][0].Metrics.TotalComponentsMemStatsAllocBytes
+                  )}
+                </span>
+              ) : (
+                renderZeroSpans
+              )}
+            </td>
+          </tr>
+          <tr>
+            <td className="sidebar--border">
+              <span>Allocs bytes vtgate</span>
+            </td>
+            <td className="datatd">
+              {data[1] ? (
+                <span>
+                  {formatByteForGB(
+                    data[1][0].Metrics.ComponentsMemStatsAllocBytes.vtgate
+                  )}
+                </span>
+              ) : (
+                renderZeroSpans
+              )}
+            </td>
+          </tr>
+          <tr>
+            <td className="sidebar--border">
+              <span>Allocs bytes vttablet</span>
+            </td>
+            <td className="datatd">
+              {data[1] ? (
+                <span>
+                  {formatByteForGB(
+                    data[1][0].Metrics.ComponentsMemStatsAllocBytes.vttablet
+                  )}
+                </span>
+              ) : (
+                renderZeroSpans
+              )}
+            </td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   );
 };
@@ -61,7 +257,7 @@ const SearchMacroDesktop = ({ data }) => {
 SearchMacroDesktop.propTypes = {
   data: PropTypes.arrayOf(
     PropTypes.oneOfType([
-      PropTypes.string, 
+      PropTypes.string,
       PropTypes.arrayOf(
         PropTypes.shape({
           Result: PropTypes.shape({
--- website/src/pages/Search/Search.jsx
+++ website/src/pages/Search/Search.jsx
@@ -17,15 +17,12 @@ limitations under the License.
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import RingLoader from "react-spinners/RingLoader";
-import { Swiper, SwiperSlide } from "swiper/react";
 import useApiCall from "../../utils/Hook";
 
 import "../Search/search.css";
 import "swiper/css";
 import "swiper/css/pagination";
 
-import { Mousewheel, Pagination, Keyboard } from "swiper";
-import SearchMacro from "../../components/SearchComponents/SearchMacro/SearchMacroMobile/SearchMacro";
 import SearchMicro from "../../components/SearchComponents/SearchMicro/SearchMicro";
 import SearchMacroDesktop from "../../components/SearchComponents/SearchMacro/SearchMacroDesktop/SearchMacroDesktop";
 
@@ -44,7 +41,7 @@ const Search = () => {
   } = useApiCall(`${import.meta.env.VITE_API_URL}search?git_ref=${gitRef}`, [
     isFormSubmitted,
   ]);
-  
+
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -95,80 +92,8 @@ const Search = () => {
           ) : (
             <>
               <div className="search__macro justify--content ">
-                <div className="searchSidebar flex--column">
-                  <span>QPS Total</span>
-                  <figure className="macrobench__Sidebar__line"></figure>
-                  <span>QPS Reads</span>
-                  <figure className="macrobench__Sidebar__line"></figure>
-                  <span>QPS Writes</span>
-                  <figure className="macrobench__Sidebar__line"></figure>
-                  <span>QPS Other</span>
-                  <figure className="macrobench__Sidebar__line"></figure>
-                  <span>TPS</span>
-                  <figure className="macrobench__Sidebar__line"></figure>
-                  <span>Latency</span>
-                  <figure className="macrobench__Sidebar__line"></figure>
-                  <span>Errors</span>
-                  <figure className="macrobench__Sidebar__line"></figure>
-                  <span>Reconnects</span>
-                  <figure className="macrobench__Sidebar__line"></figure>
-                  <span>Time</span>
-                  <figure className="macrobench__Sidebar__line"></figure>
-                  <span>Threads</span>
-                  <figure className="macrobench__Sidebar__line"></figure>
-                  <span>Total CPU time</span>
-                  <figure className="macrobench__Sidebar__line"></figure>
-                  <span>CPU time vtgate</span>
-                  <figure className="macrobench__Sidebar__line"></figure>
-                  <span>CPU time vttablet</span>
-                  <figure className="macrobench__Sidebar__line"></figure>
-                  <span>Total Allocs bytes</span>
-                  <figure className="macrobench__Sidebar__line"></figure>
-                  <span>Allocs bytes vtgate</span>
-                  <figure className="macrobench__Sidebar__line"></figure>
-                  <span>Allocs bytes vttablet</span>
-                </div>
                 <div className="searchMacro__desktop">
-                  
-                    <div className="searchMacro__desktop__header flex">
-                      {tableHeaders.map((header, index) => (
-                        <h3 key={index}>{header}</h3>
-                      ))}
-                    </div>
-                    <div className="flex searchMacro__desktop__tbody">
-                      {dataSearch.Macros &&
-                        typeof dataSearch.Macros === "object" &&
-                        Object.entries(dataSearch.Macros).map(function (
-                          searchMacro,
-                          index
-                        ) {
-                          return (
-                            <SearchMacroDesktop
-                              key={index}
-                              data={searchMacro}
-                            />
-                          );
-                        })}
-                    </div>
-                  
-                </div>
-                <div className="search__carousel__containerMobile hidden--desktop">
-                  <Swiper
-                    direction={"vertical"}
-                    slidesPerView={1}
-                    spaceBetween={30}
-                    mousewheel={true}
-                    keyboard={{
-                      enabled: true,
-                    }}
-                    pagination={{
-                      clickable: true,
-                    }}
-                    modules={[Mousewheel, Pagination, Keyboard]}
-                    onSlideChange={handleSlideChange}
-                    initialSlide={currentSlideIndex}
-                    className="mySwiper"
-                  >
+                  <div className="flex searchMacro__desktop__tbody flex--column">
                     {dataSearch.Macros &&
                       typeof dataSearch.Macros === "object" &&
                       Object.entries(dataSearch.Macros).map(function (
@@ -176,29 +101,27 @@ const Search = () => {
                         index
                       ) {
                         return (
-                          <SwiperSlide key={index}>
-                            <SearchMacro data={searchMacro} />
-                          </SwiperSlide>
+                          <SearchMacroDesktop key={index} data={searchMacro} gitRef={gitRef} />
                         );
                       })}
-                  </Swiper>
+                  </div>
                 </div>
               </div>
               <div className="search__micro">
-              {dataSearch.Micro && typeof dataSearch.Micro === "object" && (
-                <>
-                <div className="micro__thead space--between">
-                  <span className="width--12em">Package</span>
-                  <span className="width--14em">Benchmark Name</span>
-                  <span className="width--18em hiddenMobile">
-                    Number of Iterations
-                  </span>
-                  <span className="width--18em hiddenTablet">Time/op</span>
-                  <span className="width--6em">More</span>
-                </div>
-                <figure className="micro__thead__line"></figure>
-                </>
-              )}
+                {dataSearch.Micro && typeof dataSearch.Micro === "object" && (
+                  <>
+                    <div className="micro__thead space--between">
+                      <span className="width--12em">Package</span>
+                      <span className="width--14em">Benchmark Name</span>
+                      <span className="width--18em hiddenMobile">
+                        Number of Iterations
+                      </span>
+                      <span className="width--18em hiddenTablet">Time/op</span>
+                      <span className="width--6em">More</span>
+                    </div>
+                    <figure className="micro__thead__line"></figure>
+                  </>
+                )}
                 {dataSearch.Micro &&
                   typeof dataSearch.Micro === "object" &&
                   Object.entries(dataSearch.Micro).map(function (
