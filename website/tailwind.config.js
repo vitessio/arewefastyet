9cb43d8fb59c76571484a6e68dff99d968494d34
Spandan Barve <spandan567@gmail.com>
2023-09-13 21:05:35 +0530

    initial tailwind config
    - font moved to index.css
    - declared variables in tailwind config
    - declared font in tailwind config
    - added default styling to base layer
    - defined themes - default and dark

--- website/tailwind.config.js
+++ website/tailwind.config.js
@@ -2,7 +2,33 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "rgb(var(--color-primary) / <alpha-value>)",
+        secondary: "rgb(var(--color-secondary) / <alpha-value>)",
+        accent: "rgb(var(--color-accent) / <alpha-value>)",
+        foreground: "rgb(var(--color-foreground) / <alpha-value>)",
+        background: "rgb(var(--color-background) / <alpha-value>)",
+        front: "rgb(var(--color-front) / <alpha-value>)",
+        back: "rgb(var(--color-back) / <alpha-value>)",
+      },
+      screens: {
+        mobile: { max: "780px" },
+        widescreen: { min: "780px" },
+      },
+      fontFamily: {
+        opensans: '"Open Sans", sans-serif',
+      },
+      borderRadius: {
+        inherit: "inherit",
+      },
+      transitionDuration: {
+        inherit: "inherit",
+      },
+      zIndex: {
+        1: 1,
+      },
+    },
   },
   plugins: [],
 };
