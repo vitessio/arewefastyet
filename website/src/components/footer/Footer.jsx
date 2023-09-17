2d0ecdd1a7dbdc3e1d3d0c319e689adb68529042
Spandan Barve <spandan567@gmail.com>
2023-09-17 15:38:33 +0530

    Redesigned and Migrated Footer to tailwind

--- website/src/components/footer/Footer.jsx
+++ website/src/components/footer/Footer.jsx
@@ -17,74 +17,95 @@ limitations under the License.
 import React from "react";
 
 import "./footer.css";
-import logo from "../../assets/logo.png";
 import {
   AiFillGithub,
   AiFillSlackCircle,
   AiFillTwitterCircle,
 } from "react-icons/ai";
 import { BsStackOverflow } from "react-icons/bs";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
-  const icons = [
+  const socials = [
     { url: "https://github.com/vitessio/arewefastyet", icon: AiFillGithub },
     { url: "https://vitess.io/slack", icon: AiFillSlackCircle },
     { url: "https://twitter.com/vitessio", icon: AiFillTwitterCircle },
     { url: "https://stackoverflow.com/search?q=vitess", icon: BsStackOverflow },
   ];
 
+  const items = [
+    {
+      title: "Benchmarks",
+      links: [
+        { title: "Daily", to: "/daily" },
+        { title: "Compare", to: "/compare" },
+        { title: "Micro", to: "/micro" },
+        { title: "Macro", to: "/macro" },
+        { title: "PR", to: "/pr" },
+      ],
+    },
+    {
+      title: "Vitess",
+      links: [
+        { title: "Docs", to: "https://vitess.io/docs" },
+        { title: "Blog", to: "https://vitess.io/blog" },
+        { title: "Community", to: "https://vitess.io/community" },
+      ],
+    },
+    {
+      title: "arewefastyet",
+      links: [{ title: "github", to: "https://www.github.com/vitessio/arewefastyet" }],
+    },
+  ];
+
   return (
-    <div className="footer__main">
-      <div className="footer">
-        <div className="footer__one">
-          <div className="footer__company">
-            <img src={logo} alt="logo" className="footer__logo" />
-            <h1>arewefastyet</h1>
-          </div>
-          <div className="footer__icons">
-            {icons.map((item, index) => (
-              <a key={index} href={item.url} target="_blank">
-                <item.icon size={33} />
-              </a>
-            ))}
-          </div>
+    <footer className="p-page relative mt-10 py-20 text-back">
+      <div className="absolute bottom-6 left-6 right-6 top-6 -z-1 rounded-lg bg-primary" />
+      <div className="flex justify-between text-sm font-light tracking-tight">
+        <div
+          className={`flex basis-[20%] flex-col items-center gap-y-5 opacity-90 drop-shadow-[0px_0px_0px_theme("colors.back")]`}
+        >
+          <img
+            src="/logo.png"
+            alt="agrosurance logo"
+            className="aspect-square w-1/3"
+          />
+          <h2 className="text-xl font-medium">arewefastyet</h2>
         </div>
-        <div className="links">
-          <div className="footer__usefulLinks">
-            <h2>Vitess</h2>
-            <div className="">
-              <a href="https://vitess.io/docs" target="_blank">
-                <p>Docs</p>
-              </a>
-              <a href="https://vitess.io/blog" target="_blank">
-                <p>Blog</p>
-              </a>
-              <a href="https://vitess.io/community" target="_blank">
-                <p>Community</p>
-              </a>
+        {items.map((item, key) => (
+          <div key={key} className="flex flex-col">
+            <h5 className="font-semibold">{item.title}</h5>
+            <div className="my-7 flex flex-col gap-y-3">
+              {item.links.map((link, key) => (
+                <Link key={key} to={link.to}>{link.title}</Link>
+              ))}
             </div>
           </div>
-          <div className="footer__usefulLinks">
-            <a href="https://github.com/vitessio/arewefastyet" target="_blank">
-              <h2>arewefastyet</h2>
-            </a>
-            <div className="">
-              <a
-                href="https://github.com/vitessio/arewefastyet"
-                target="_blank"
-              >
-                <p>GitHub</p>
-              </a>
-            </div>
-          </div>
+        ))}
+
+        <div className="flex flex-col items-center text-center text-back text-opacity-80">
+          <h5 className="my-3 text-4xl font-bold tracking-tighter text-back">
+            TRULY TESTED
+          </h5>
+          <p>A cutting edge Benchmarking</p>
+          <p>approach for uparalleled</p>
+          <p>Database Speed</p>
+          <p className="font-semibold text-back">for vitess</p>
         </div>
       </div>
-      <div>
-        <p className="copyrightText__footer justify--content">
-          Copyright © 2023
-        </p>
+      <div className="my-2 flex gap-x-4">
+        <p className="font-mono text-xl font-bold">Follow Us</p>
+        <div className="flex items-center gap-x-3">
+          {socials.map((social, key) => (
+            <Link key={key} to={social.url} target="__blank" className="">
+              <social.icon size={33} />
+            </Link>
+          ))}
+        </div>
       </div>
-    </div>
+      <div className="my-2 w-full border border-back"></div>
+      <div className="mt-4 text-xs">@vitessio/arewefastyet</div>
+    </footer>
   );
 };
 
