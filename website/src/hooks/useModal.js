3401213234e68a0b8c72323151264c2ec1079fde
Spandan Barve <spandan567@gmail.com>
2023-10-15 02:59:56 +0530

    added global context and hooks useTheme and useModal

---
+++ website/src/hooks/useModal.js
@@ -0,0 +1,15 @@
+import useGlobalContext from "../context/GlobalContext";
+
+export default function useModal() {
+  const { modal, setModal } = useGlobalContext();
+
+  function hide() {
+    setModal(null);
+  }
+
+  function show(element) {
+    setModal(element);
+  }
+
+  return { element: modal, show, hide };
+}
---
+++ website/src/hooks/useTheme.js
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import useGlobalContext from "../context/GlobalContext";
+import resolveConfig from "tailwindcss/resolveConfig";
+import tailwindConfig from "../../tailwind.config.js";
+
+function getThemeColors() {
+  const twConf = resolveConfig(tailwindConfig);
+  const colors = twConf.theme.colors;
+
+  return {
+    primary: colors.primary,
+    secondary: colors.secondary,
+    accent: colors.accent,
+    front: colors.front,
+    back: colors.back,
+    background: colors.background,
+    foreground: colors.foreground,
+  };
+}
+
+export default function useTheme() {
+  const { theme, setTheme } = useGlobalContext();
+
+  const [colors, setColors] = useState(getThemeColors());
+
+  function set(newTheme) {
+    setTheme(newTheme);
+    setColors(getThemeColors());
+  }
+
+  return { current: theme, set, colors };
+}
