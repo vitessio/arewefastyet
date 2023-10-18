f5fd7f351288618efff278c1df32cac30d1221ba
Spandan Barve <spandan567@gmail.com>
2023-10-19 00:54:08 +0530

    homepage hero -> changed slogan and changed button to Link

--- website/src/pages/HomePage/components/Hero.jsx
+++ website/src/pages/HomePage/components/Hero.jsx
@@ -16,8 +16,7 @@ export default function Hero() {
             <h3 className="text-4xl font-semibold">Vitess Introduces</h3>
             <h1 className="text-6xl font-bold text-primary">arewefastyet</h1>
             <p className="text-lg font-normal mt-5 whitespace-nowrap">
-              A Cutting-Edge Benchmarking Approach for Unparalleled Database
-              Speed
+              A Benchmarking System for Vitess
             </p>
           </div>
           <div className="flex gap-x-8">
@@ -28,19 +27,26 @@ export default function Hero() {
             >
               Read our blog post <i className="fa-solid fa-bookmark"></i>
             </Link>
-            <button
+            <Link
               className="bg-black text-white rounded-2xl p-5 flex items-center gap-x-2"
               to="https://github.com/vitessio/arewefastyet"
               target="__blank"
             >
               Contribute on GitHub
               <i className="fa-brands fa-github"></i>
-            </button>
+            </Link>
           </div>
         </div>
       </div>
       <div className="basis-1/3 flex justify-start items-center">
-        <img src="/logo.png" alt="logo" className="w-11/12" style={{filter : 'drop-shadow(10px 10px 20px rgb(var(--color-primary))'}} />
+        <img
+          src="/logo.png"
+          alt="logo"
+          className="w-11/12"
+          style={{
+            filter: "drop-shadow(10px 10px 20px rgb(var(--color-primary))",
+          }}
+        />
       </div>
     </section>
   );
