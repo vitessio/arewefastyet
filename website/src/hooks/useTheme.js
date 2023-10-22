14ab07045b285284394d13c9a7feabe7e9aa5ed4
Spandan Barve <spandan567@gmail.com>
2023-10-23 02:35:22 +0530

    remove color state from useTheme due to multiple recalls and redeclaration of state

--- website/src/hooks/useTheme.js
+++ website/src/hooks/useTheme.js
@@ -1,32 +1,12 @@
-import { useState } from "react";
 import useGlobalContext from "../contexts/GlobalContext";
-import resolveConfig from "tailwindcss/resolveConfig";
-import tailwindConfig from "../../tailwind.config.js";
-
-function getThemeColors() {
-  const twConf = resolveConfig(tailwindConfig);
-  const colors = twConf.theme.colors;
-
-  return {
-    primary: colors.primary,
-    secondary: colors.secondary,
-    accent: colors.accent,
-    front: colors.front,
-    back: colors.back,
-    background: colors.background,
-    foreground: colors.foreground,
-  };
-}
 
 export default function useTheme() {
   const { theme, setTheme } = useGlobalContext();
-
-  const [colors, setColors] = useState(getThemeColors());
 
   function set(newTheme) {
     setTheme(newTheme);
     setColors(getThemeColors());
   }
 
-  return { current: theme, set, colors };
+  return { current: theme, set };
 }
