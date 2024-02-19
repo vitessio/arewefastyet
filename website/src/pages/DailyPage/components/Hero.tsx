e4f53ad33dcf712938940758a22f4ab1841ded63
Manik Rana <manikrana54@gmail.com>
2024-02-19 13:24:33 +0530

    feat: make daily page hero responsive

--- website/src/pages/DailyPage/components/Hero.tsx
+++ website/src/pages/DailyPage/components/Hero.tsx
@@ -18,15 +18,15 @@ import React from "react";
 export default function Hero() {
   return (
     <section className="flex h-[60vh] pt-[8vh] items-center p-page">
-      <div className="flex basis-1/2 flex-col">
+      <div className="flex flex-col justify-start items-start gap-5 max-w-screen-md">
         <h2 className="text-8xl text-primary">Daily</h2>
-        <p className="my-6 leading-loose">
+        <p className="md:my-6 leading-loose">
           We run all macro benchmark workloads against the <i>main</i> branch
           every day. This is done to ensure the consistency of the results over
-          time on <i>main</i>. On this page, you can find graphs that show you the
-          results of all five macro benchmark workload over the last 30 days.
-          Click on a macro benchmark workload to see all the results for that
-          workload.
+          time on <i>main</i>. On this page, you can find graphs that show you
+          the results of all five macro benchmark workload over the last 30
+          days. Click on a macro benchmark workload to see all the results for
+          that workload.
         </p>
       </div>
     </section>
