31ace76fb49526479ab54f01a2212f9ecf627e94
Jad Chahed <jad.chahed@bashroom.com>
2024-07-12 18:16:25 +0200

    refactor: hero
    Signed-off-by: Jad Chahed <jad.chahed@bashroom.com>

--- website/src/pages/StatusPage/components/StatusHero.tsx
+++ website/src/pages/StatusPage/components/StatusHero.tsx
@@ -14,16 +14,34 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-import useApiCall from "@/utils/Hook";
-import { statusDataTypes } from "@/types";
 import Hero, { HeroProps } from "@/common/Hero";
-import { Card, CardHeader, CardTitle, CardFooter } from "@/components/ui/card";
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import {
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+import useApiCall from "@/utils/Hook";
+import { Bar, BarChart, CartesianGrid, XAxis, YAxis } from "recharts";
 
 const info = [
-  { title: "Total Benchmarks", content: "Total" },
-  { title: "Successful", content: "Finished" },
-  { title: `Total (last 30 days)`, content: "Last30Days" },
+  { title: "Benchmark in total", content: "Total" },
+  { title: "Benchmark this month", content: "Last30Days" },
+  { title: `Commits Benchmarked`, content: "Commits" },
 ];
+
+type DataStatusType = {
+  Total: number;
+  Last30Days: number;
+  Last7Days: number[];
+  Commits: number;
+};
 
 const heroProps: HeroProps = {
   title: "Status",
@@ -31,17 +49,32 @@ const heroProps: HeroProps = {
     <>
       Arewefastyet has a single execution queue with tasks that are executed
       sequentially on our benchmarking server. Each execution has a status that
-      can be either: started, failed or finished. When a benchmark is marked as
-      finished it means that it ran successfully.
+      can be either: <b>started</b>started, <b>failed</b> or <b>finished</b>.
+      When a benchmark is marked as finished it means that it ran successfully.
     </>
   ),
 };
 
 export default function StatusHero() {
-  const { data: dataStatusStats } = useApiCall<statusDataTypes>(
+  const { data: dataStatusStats } = useApiCall<DataStatusType>(
     `${import.meta.env.VITE_API_URL}status/stats`
   );
 
+  console.log({ dataStatusStats });
+
+  const chartConfig = {
+    day: {
+      label: "Day",
+      color: "hsl(var(--chart-1))",
+    },
+  };
+
+  const last7days = [234, 283, 210, 371, 234, 283, 210];
+
+  const chartData = last7days.map((commits, index) => ({
+    day: index.toString(),
+    commits,
+  }));
 
   return (
     <Hero title={heroProps.title} description={heroProps.description}>
@@ -49,19 +82,43 @@ export default function StatusHero() {
         {info.map(({ content, title }, key) => (
           <Card
             key={key}
-            className="w-[214px] h-fit md:min-h-fit border-border"
+            className="w-[300px] h-[150px] md:min-h-[130px] border-border"
           >
             <CardHeader>
               <CardTitle
                 className="counter text-3xl md:text-5xl text-primary"
-                style={{ "--num": dataStatusStats[content] }}
+                style={{ ["--num" as string]: dataStatusStats[content] }}
               ></CardTitle>
             </CardHeader>
-            <CardFooter className="text-xs md:text-lg font-light md:font-medium">
+            <CardFooter className="text-xs md:text-xl font-light md:font-medium">
               {title}
             </CardFooter>
           </Card>
         ))}
+        <Card className=" w-[300px] max-h-[150px] border-border">
+          <CardContent className="w-[300px] max-h-[150px]">
+            <ChartContainer config={chartConfig}>
+              <BarChart
+                margin={{ top: 12, right: 0, left: -32, bottom: 0 }}
+                barSize={10}
+                accessibilityLayer
+                data={chartData}
+              >
+                <YAxis tickMargin={0} />
+                <XAxis tickMargin={0} tick={false} />
+                <CartesianGrid vertical={false} />
+                <ChartTooltip
+                  cursor={true}
+                  content={<ChartTooltipContent hideLabel />}
+                />
+                <Bar dataKey="commits" fill="var(--color-day)" />
+              </BarChart>
+            </ChartContainer>
+          </CardContent>
+          <CardFooter className="text-xs text-primary flex justify-center md:text-sm font-light md:font-medium mt-[-28px]">
+            Benchmarks over the last 7 days
+          </CardFooter>
+        </Card>
       </div>
     </Hero>
   );
