20bb3498f9f16b5063bf4376161beb67e715d9e8
Florent Poinsard <florent.poinsard@outlook.fr>
2024-07-24 14:29:39 -0600

    Remove old cron service
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>

--- infra/cleanup/cleanup_exec.sh
+++ infra/cleanup/cleanup_exec.sh
@@ -5,6 +5,6 @@ EXEC_DIR="/exec"
 
 # Ensure the directory exists and is not empty
 if [ -d "$EXEC_DIR" ] && [ "$(ls -A $EXEC_DIR)" ]; then
-  # Find and delete directories older than 1 minute
+  # Find and delete directories that are 7-days old or older
   find "$EXEC_DIR" -mindepth 1 -type d -mtime +6 -exec rm -rf {} +
 fi
