1b68e693a01ea685ebbf1b01e807764513da7e1f
Florent Poinsard <florent.poinsard@outlook.fr>
2024-07-25 12:50:24 -0600

    Add read-only psdb connections
    Signed-off-by: Florent Poinsard <florent.poinsard@outlook.fr>

--- go/storage/psdb/psdb.go
+++ go/storage/psdb/psdb.go
@@ -22,91 +22,130 @@ import (
 	"database/sql"
 	"errors"
 	"fmt"
+
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 )
 
 const (
-	flagPsdbOrg      = "planetscale-db-org"
-	flagPsdbPassword = "planetscale-db-password"
-	flagPsdbUser     = "planetscale-db-user"
-	flagPsdbHost     = "planetscale-db-host"
-	flagPsdbDatabase = "planetscale-db-database"
-	flagPsdbBranch   = "planetscale-db-branch"
+	flagPsdbOrg           = "planetscale-db-org"
+	flagPsdbPasswordWrite = "planetscale-db-password-write"
+	flagPsdbUserWrite     = "planetscale-db-user-write"
+	flagPsdbPasswordRead  = "planetscale-db-password-read"
+	flagPsdbUserRead      = "planetscale-db-user-read"
+	flagPsdbHost          = "planetscale-db-host"
+	flagPsdbDatabase      = "planetscale-db-database"
+	flagPsdbBranch        = "planetscale-db-branch"
 
 	ErrorClientConnectionNotInitialized = "the client connection to the database is not initialized"
 )
 
 type (
+	auth struct {
+		username string
+		password string
+	}
+
 	Config struct {
-		Org      string
-		Database string
-		Branch   string
-		User string
-		Password string
-		Host string
+		Org       string
+		Database  string
+		Branch    string
+		Host      string
+		authWrite auth
+		authRead  auth
 	}
 
 	Client struct {
-		Config *Config
-		dial   *sql.DB
+		Config  *Config
+		writeDB *sql.DB
+		readDB  *sql.DB
 	}
 )
 
-func (cfg Config) IsValid() bool {
-	return !(cfg.User == "" || cfg.Password == "" || cfg.Host == "" || cfg.Org == "" || cfg.Database == "" || cfg.Branch == "")
+func (au auth) isValid() bool {
+	return au.username != "" && au.password != ""
+}
+
+func (cfg *Config) IsValid() bool {
+	return cfg.Org != "" && cfg.Database != "" && cfg.Branch != "" && cfg.Host != "" && cfg.authWrite.isValid() && cfg.authRead.isValid()
 }
 
 func (cfg *Config) AddToViper(v *viper.Viper) {
+	// General settings
 	_ = v.UnmarshalKey(flagPsdbOrg, &cfg.Org)
 	_ = v.UnmarshalKey(flagPsdbHost, &cfg.Host)
-	_ = v.UnmarshalKey(flagPsdbPassword, &cfg.Password)
-	_ = v.UnmarshalKey(flagPsdbUser, &cfg.User)
 	_ = v.UnmarshalKey(flagPsdbDatabase, &cfg.Database)
 	_ = v.UnmarshalKey(flagPsdbBranch, &cfg.Branch)
+
+	// Write authentication
+	_ = v.UnmarshalKey(flagPsdbPasswordWrite, &cfg.authWrite.password)
+	_ = v.UnmarshalKey(flagPsdbUserWrite, &cfg.authWrite.username)
+
+	// Read authentication
+	_ = v.UnmarshalKey(flagPsdbPasswordWrite, &cfg.authWrite.password)
+	_ = v.UnmarshalKey(flagPsdbUserWrite, &cfg.authWrite.username)
 }
 
 func (cfg *Config) AddToCommand(cmd *cobra.Command) {
+	// General settings
 	cmd.Flags().StringVar(&cfg.Org, flagPsdbOrg, "", "Name of the PlanetscaleDB organization.")
-	cmd.Flags().StringVar(&cfg.User, flagPsdbUser, "", "Username used to authenticate to PlanetscaleDB.")
-	cmd.Flags().StringVar(&cfg.Password, flagPsdbPassword, "", "Password used to authenticate to PlanetscaleDB.")
 	cmd.Flags().StringVar(&cfg.Host, flagPsdbHost, "", "Hostname of the PlanetscaleDB database.")
 	cmd.Flags().StringVar(&cfg.Database, flagPsdbDatabase, "", "PlanetscaleDB database name.")
 	cmd.Flags().StringVar(&cfg.Branch, flagPsdbBranch, "main", "PlanetscaleDB branch to use.")
-
 	_ = viper.BindPFlag(flagPsdbOrg, cmd.Flags().Lookup(flagPsdbOrg))
 	_ = viper.BindPFlag(flagPsdbHost, cmd.Flags().Lookup(flagPsdbHost))
-	_ = viper.BindPFlag(flagPsdbUser, cmd.Flags().Lookup(flagPsdbUser))
-	_ = viper.BindPFlag(flagPsdbPassword, cmd.Flags().Lookup(flagPsdbPassword))
 	_ = viper.BindPFlag(flagPsdbDatabase, cmd.Flags().Lookup(flagPsdbDatabase))
 	_ = viper.BindPFlag(flagPsdbBranch, cmd.Flags().Lookup(flagPsdbBranch))
+
+	// Write authentication
+	cmd.Flags().StringVar(&cfg.authWrite.username, flagPsdbUserWrite, "", "Username used to authenticate to the write servers of PlanetScaleDB.")
+	cmd.Flags().StringVar(&cfg.authWrite.password, flagPsdbPasswordWrite, "", "Password used to authenticate to the write servers of PlanetScaleDB.")
+	_ = viper.BindPFlag(flagPsdbUserWrite, cmd.Flags().Lookup(flagPsdbUserWrite))
+	_ = viper.BindPFlag(flagPsdbPasswordWrite, cmd.Flags().Lookup(flagPsdbPasswordWrite))
+
+	// Read authentication
+	cmd.Flags().StringVar(&cfg.authRead.username, flagPsdbUserRead, "", "Username used to authenticate to the read-only servers of PlanetScaleDB.")
+	cmd.Flags().StringVar(&cfg.authRead.password, flagPsdbPasswordRead, "", "Password used to authenticate to the read-only servers of PlanetScaleDB.")
+	_ = viper.BindPFlag(flagPsdbUserRead, cmd.Flags().Lookup(flagPsdbUserRead))
+	_ = viper.BindPFlag(flagPsdbPasswordRead, cmd.Flags().Lookup(flagPsdbPasswordRead))
 }
 
-func (cfg Config) NewClient() (*Client, error) {
-	client := &Client{
-		Config: &cfg,
-	}
+func (cfg *Config) connectionString(a auth) string {
+	return fmt.Sprintf("%s:%s@tcp(%s)/%s?parseTime=true&tls=true&interpolateParams=true", a.username, a.password, cfg.Host, cfg.Database)
+}
 
-	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/%s?parseTime=true&tls=true", cfg.User, cfg.Password, cfg.Host, cfg.Database))
+func (cfg *Config) NewClient() (*Client, error) {
+	// Open a connection pool to the write servers
+	writedb, err := sql.Open("mysql", cfg.connectionString(cfg.authWrite))
 	if err != nil {
 		return nil, err
 	}
-	client.dial = db
-	return client, nil
+
+	// Open a connection pool to the read-only servers
+	readdb, err := sql.Open("mysql", cfg.connectionString(cfg.authRead))
+	if err != nil {
+		return nil, err
+	}
+
+	return &Client{
+		Config:  cfg,
+		writeDB: writedb,
+		readDB:  readdb,
+	}, nil
 }
 
 func (c *Client) Close() error {
-	if c.dial == nil {
+	if c.writeDB == nil {
 		return errors.New(ErrorClientConnectionNotInitialized)
 	}
-	return c.dial.Close()
+	return c.writeDB.Close()
 }
 
 func (c *Client) Insert(query string, args ...interface{}) (int64, error) {
-	if c.dial == nil {
+	if c.writeDB == nil {
 		return 0, errors.New(ErrorClientConnectionNotInitialized)
 	}
-	stms, err := c.dial.Prepare(query)
+	stms, err := c.writeDB.Prepare(query)
 	if err != nil {
 		return 0, err
 	}
@@ -120,10 +159,10 @@ func (c *Client) Insert(query string, args ...interface{}) (int64, error) {
 }
 
 func (c *Client) Select(query string, args ...interface{}) (*sql.Rows, error) {
-	if c.dial == nil {
+	if c.readDB == nil {
 		return nil, errors.New(ErrorClientConnectionNotInitialized)
 	}
-	rows, err := c.dial.Query(query, args...)
+	rows, err := c.readDB.Query(query, args...)
 	if err != nil {
 		return nil, err
 	}
