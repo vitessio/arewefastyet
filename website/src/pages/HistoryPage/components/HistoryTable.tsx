f76289a36419228999d29b8a2bba261c35c339ae
Jad Chahed <jad.chahed@bashroom.com>
2024-07-25 18:37:05 +0200

    refactor: no result found message
    Signed-off-by: Jad Chahed <jad.chahed@bashroom.com>

--- website/src/pages/HistoryPage/components/HistoryTable.tsx
+++ website/src/pages/HistoryPage/components/HistoryTable.tsx
@@ -134,7 +134,7 @@ export function HistoryTable<TData, TValue>({
                   colSpan={columns.length}
                   className="h-24 text-center"
                 >
-                  No results.
+                  No executions or benchmarks found for this commit.
                 </TableCell>
               </TableRow>
             )}
